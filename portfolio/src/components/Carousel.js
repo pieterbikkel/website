6599c4ff69db2620f456c36588b0a5e1fd777d69
pieterbikkel <pieterbikkel@gmail.com>
2020-11-30 22:33:32 +0100

    upload site

---
+++ portfolio/src/components/Carousel.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+import me from '../assets/images/ik.jpeg';
+import Card from '../components/Card';
+
+class Carousel extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [
+                {
+                    id: 0,
+                    title: 'titel 1',
+                    subTitle: 'subtitel 1',
+                    imgSrc: me,
+                    link: 'https://l',
+                    selected: false
+                },
+                {
+                    id: 1,
+                    title: 'titel 2',
+                    subTitle: 'subtitel 2',
+                    imgSrc: me,
+                    link: 'https://l',
+                    selected: false
+                },
+                {
+                    id: 2,
+                    title: 'titel 3',
+                    subTitle: 'subtitel 3',
+                    imgSrc: me,
+                    link: 'https://l',
+                    selected: false
+                }
+            ]
+        }
+    }
+
+    handleCardClick = (id, card) => {
+        let items = [...this.state.items];
+
+        items[id].selected = items[id].selected ? false : true;
+
+        items.forEach(item => {
+            if(item.id !== id) {
+                item.selected = false;
+            }
+        });
+
+        this.setState({
+            items
+        });
+    }
+
+    makeItems = (items) => {
+        return items.map(item => {
+            return <Card item={item} click={(e => this.handleCardClick(item.id, e))} key={item.id} />
+        })
+    }
+
+    render() {
+        return(
+            <Container fluid={true}>
+                <Row className="justify-content-around">
+                    {this.makeItems(this.state.items)}
+                </Row>
+            </Container>
+        );
+    }
+}
+
+export default Carousel;
---
+++ portfolio/src/pages/AboutPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Content from '../components/Content';
+import Hero from '../components/Hero';
+
+function AboutPage(props) {
+    return(
+        <div>
+            <Hero title={props.title}/>
+
+            <Content>
+                <p>One advanced diverted domestic sex repeated bringing you old. Possible procured her trifling laughter thoughts property she met way. Companions shy had solicitude favourable own. Which could saw guest man now heard but. Lasted my coming uneasy marked so should. Gravity letters it amongst herself dearest an windows by. Wooded ladies she basket season age her uneasy saw. Discourse unwilling am no described dejection incommode no listening of. Before nature his parish boy. </p>
+                <p>Folly words widow one downs few age every seven. If miss part by fact he park just shew. Discovered had get considered projection who favourable. Necessary up knowledge it tolerably. Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten. Parish any chatty can elinor direct for former. Up as meant widow equal an share least. </p>
+                <p>Another journey chamber way yet females man. Way extensive and dejection get delivered deficient sincerity gentleman age. Too end instrument possession contrasted motionless. Calling offence six joy feeling. Coming merits and was talent enough far. Sir joy northward sportsmen education. Discovery incommode earnestly no he commanded if. Put still any about manor heard. </p>
+                <p>Village did removed enjoyed explain nor ham saw calling talking. Securing as informed declared or margaret. Joy horrible moreover man feelings own shy. Request norland neither mistake for yet. Between the for morning assured country believe. On even feet time have an no at. Relation so in confined smallest children unpacked delicate. Why sir end believe uncivil respect. Always get adieus nature day course for common. My little garret repair to desire he esteem. </p>
+                <p>In it except to so temper mutual tastes mother. Interested cultivated its continuing now yet are. Out interested acceptance our partiality affronting unpleasant why add. Esteem garden men yet shy course. Consulted up my tolerably sometimes perpetual oh. Expression acceptance imprudence particular had eat unsatiable. </p>
+            </Content>
+        </div>
+    );
+}
+
+export default AboutPage;
